533b8b0456fa150198b976dee0f5ad87da8919f8
avabork <165568759+avabork@users.noreply.github.com>
2025-10-24 12:54:55 +0530

    feat: Configure app for production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import connectDB from './config/db.js';
 import reportRoutes from './routes/report.routes.js';
@@ -12,19 +13,12 @@ connectDB();
 
 const app = express();
 
-// Middleware
-// ...other imports
-import cors from 'cors';
-
-// --- UPDATE CORS ---
-// We will set the allowed origin from an environment variable
+// Set the allowed origin from an environment variable
 const frontendURL = process.env.FRONTEND_URL || 'http://localhost:3000';
 app.use(cors({
   origin: frontendURL
 }));
-// --- END OF UPDATE ---
 
-// ...rest of the file
 app.use(express.json()); // To accept JSON data
 
 // API Routes
